5f9500f952bda1b61b1980c688baf23b5ae83087
Akshansh Patel <akshanshpatel003@gmail.com>
2025-11-05 12:42:07 +0530

    express and ejs

--- backend/express_nodemon/index.js
+++ backend/express_nodemon/index.js
@@ -1,9 +1,27 @@
 let express=require('express')
 let app=express();
+let path=require('path');
 
 // app.get('/',(req,res)=>{
 //     res.send(`<h2>Home Route betiii</h2>`)
 // })
+
+//this is ejs part
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/random',(req,res)=>{
+    let number=Math.floor(Math.random()*100) ;
+    res.render('random',{number})
+})
+
+
+
+
 
 
 //This is a better way for handling any route instead of creating one for every new req
@@ -15,14 +33,19 @@ let app=express();
 // })
 
 //this one is ? query
-app.get('/search', (req,res)=>{
-    //destructuring
-    console.log(req.query.search)
-    let {search}=req.query;
-    res.send(search);
-    // res.send(req.query);
-    // res.send(req.query.search);
-})
+// app.get('/search', (req,res)=>{
+//     //destructuring
+//     console.log(req.query.search)
+//     let {search}=req.query;
+//     res.send(search);
+//     // res.send(req.query);
+//     // res.send(req.query.search);
+// })
+
+
+
+
+
 
 app.listen(8080,()=>{
     console.log(`app running at 8080 port`)
