05182c1771afd5689189657af5e6ce85ebfd4b9a
Akshansh Patel <akshanshpatel003@gmail.com>
2025-11-09 20:56:36 +0530

    RESTful routing

--- backend/RESTful_routing/index.js
+++ backend/RESTful_routing/index.js
@@ -1,21 +1,28 @@
 const express=require('express')
 const app=express()
 const path=require('path')
+const methodOverride = require('method-override')
+const { v4:uuid } = require('uuid');  // uuid provides universally unique identity
 
+uuid();
 
 let comments=[
     {
-        id:0,
+        // id:0,
+        id:uuid(),
         name:"Akshansh",
         comment:"atmkbfjg"
     },
     {
-        id:1,
+        // id:1,
+        id:uuid(),
+
         name:"Akshay",
         comment:"Chalre Nooob"
     },
     {
-        id:2,
+        // id:2,
+        id:uuid(),
         name:"Manish",
         comment:"DSA Paglu"
     }
@@ -37,6 +44,17 @@ app.use(express.static(path.join(__dirname,'public')));
 
 //for getting data out of form otherwise its default is undefined
 app.use(express.urlencoded({extended:true}))
+
+//middleware for method overriding of post to patch
+app.use(methodOverride('_method'))
+
+
+
+
+
+
+
+
 
 //root 
 app.get('/',(req,res)=>{
@@ -63,7 +81,7 @@ app.get('/blogs/new',(req,res)=>{
 app.post('/blogs',(req,res)=>{
     // console.log(req.body)
     let{name,comment}=req.body  //destructuring
-    comments.push({name,comment,id:comments.length})
+    comments.push({name,comment,id:uuid()})
     res.redirect('/blogs')  
 })
 
@@ -73,12 +91,45 @@ app.post('/blogs',(req,res)=>{
 app.get('/blogs/:id',(req,res)=>{
     let{id}=req.params
     // console.log(id)
-    let foundComment=comments.find((comment)=>{return comment.id==id})
+    let foundComment=comments.find((comment)=>{return comment.id==id}) //return id that is a string
     // console.log(foundComment)
     
 
     res.render('show',{foundComment})
 })
+
+
+//task 5 edit ka page
+app.get('/blogs/:id/edit',(req,res)=>{
+    let{id}=req.params
+    let foundComment=comments.find((comment)=>{return comment.id==id})
+
+    res.render('edit',{foundComment});
+})
+
+
+//actually editing
+app.patch('/blogs/:id',(req,res)=>{
+    let{id}=req.params
+    let foundComment=comments.find((comment)=>{return comment.id==id})
+    let{comment}=req.body;
+    console.log(comment)
+    foundComment.comment=comment
+    res.redirect('/blogs')
+})
+
+
+
+
+//actually delete logic
+app.post('/blogs/:id',(req,res)=>{
+    let{id}=req.params;
+    let newArr=comments.filter((comment)=>{return comment.id!=id})
+    comments=newArr;
+    res.redirect('/blogs')
+})
+
+
 
 
 
